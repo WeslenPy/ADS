df9ec8e67716df8a9c38bb0535ed7a661b14edee
weslenpy <weslenjhony@gmail.com>
2025-09-20 00:29:13 -0300

    fix main

--- projeto_integrado_inovação/main.py
+++ projeto_integrado_inovação/main.py
@@ -14,6 +14,16 @@ class Utils:
         pass
     
     
+    def validar_entrada(self,value:str)->bool:
+        
+        try:
+            int(value)
+            return True
+        
+        except Exception as erro:
+            return False
+    
+    
     def limpar_terminal(self):
         if os.name == "nt":#windows
             os.system("cls")
@@ -54,7 +64,7 @@ class Clinica:
         pass
 
 
-    def analises_estatisticas(self):
+    def ver_estatisticas(self):
         pass
     
     def buscar_paciente(self):
@@ -64,15 +74,55 @@ class Clinica:
         pass
     
 
+
+
 class Menu:
     def __init__(self):
-        pass
+        
+        self.clinica = Clinica()
+        self.utils = Utils()
+        
+        
+        self.map_menus = {
+            "1": {"title":"Cadastrar pacientes","function":self.clinica.cadastrar_paciente} ,
+            "2": {"title":"Ver estatísticas","function":self.clinica.ver_estatisticas} ,
+            "3": {"title":"Buscar paciente","function":self.clinica.buscar_paciente} ,
+            "4": {"title":"Listar todos os pacientes","function":self.clinica.listar_todos_os_pacientes} ,
+            "5": {"title":"Sair","function":self.sair} ,
+        }
+        
+        
+        self.menu_montado  = self.montar_menu()
+        
+    def montar_menu(self):
+        return "\n".join(map(lambda items:f"{items[0]}. {items[1]['title']}",list(self.map_menus.items())))
     
     
-    
-    
+    def call_op(self,op:str):
+        if(str(op) in self.map_menus):
+            menu_function =  self.map_menus.get(op,{})
+            return menu_function.get("function",self.sair)()
+        
+        return None
+            
+            
+    def show(self):
+        
+        while True:
+            print(self.menu_montado)
+            op_escolhida = input("\nEscolha uma opção: ").strip()
+            if not self.utils.validar_entrada(op_escolhida):
+                print("Escolha uma opção valida.")
+                
+            
+            self.call_op(op_escolhida)                
     
     
     def sair(self):
         sys.exit(1)
+        
+        
+        
+menu = Menu()
+menu.show()
     
